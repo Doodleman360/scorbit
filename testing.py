30afb79fdaf35cd7f2f3e5b1865646586755f2f9
Doodleman360 <julislo123@gmail.com>
2023-04-06 10:18:15 -0600

    Starting code

---
+++ testing.py
@@ -0,0 +1,31 @@
+# get json data from a url
+import requests
+import json
+
+#get creds from file
+with open('creds.json') as f:
+    creds = json.load(f)
+
+
+# function to add commas to numbers
+def add_commas(n):
+    n = str(n)
+    if len(n) <= 3:
+        return n
+    return add_commas(n[:-3]) + ',' + n[-3:]
+
+
+machines = [67444, 67443, 67445]
+
+for machine in machines:
+    machineUrl = f"https://api.scorbit.io/api/venuemachine/{machine}"
+    scoreUrl = f"https://api.scorbit.io/api/venuemachine/{machine}/top_scores/"
+    machineR = requests.get(machineUrl, auth=(creds["username"], creds["password"]))
+    scoreR = requests.get(scoreUrl, auth=(creds["username"], creds["password"]))
+    machineData = machineR.json()
+    scoreData = scoreR.json()
+
+    print(f"{machineData['machine']['name']} High Scores")
+    for i in scoreData['all_time_venuemachine']:
+        print(add_commas(i['score']), i['player']['initials'])
+    print()
