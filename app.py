b35358ae58a6d63595b495f6dcfee07017106b5a
Doodleman360 <Julislo123@gmail.com>
2023-07-28 16:27:02 -0600

    testing

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def get_scores(cached=False):
             for i in localScoreData:
                 expireDate = today - timedelta(days=expireInterval)
                 if datetime.strptime(i['updated'], '%Y-%m-%dT%H:%M:%S.%fZ') < expireDate:
-                    print(i['score'])
+                    print(type(i['score']))
                     if i['score'] > topExpiredScore['score']:
                         topExpiredScore = {"score": i['score'], "initials": i['player']['initials'], "daysLeft": -(expireDate - datetime.strptime(i['updated'], '%Y-%m-%dT%H:%M:%S.%fZ')).days, "mostRecent": False, "updated": i['updated']}
                     continue
